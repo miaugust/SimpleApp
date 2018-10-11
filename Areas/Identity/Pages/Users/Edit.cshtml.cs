6a576e79ec9fea888f68b07538b98f5219711833
Miłosz Augustyński <maugustynski@ADHSJ.HSW-HSJ.PL>
2018-10-11 08:40:52 +0200

    fin

--- Areas/Identity/Pages/Users/Edit.cshtml.cs
+++ Areas/Identity/Pages/Users/Edit.cshtml.cs
@@ -2,26 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SimpleApp.Areas.Identity.Data;
+using SimpleApp.Data;
 using SimpleApp.Models;
 
 namespace SimpleApp.Areas.Identity.Pages.Users
 {
     public class EditModel : PageModel
     {
+        private readonly UserManager<SimpleAppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly AppDbContext _context;
 
-        public EditModel(AppDbContext context)
+        public EditModel(AppDbContext context, UserManager<SimpleAppUser> userManager,RoleManager<IdentityRole> roleManager)
         {
+            _userManager = userManager;
+            _roleManager = roleManager;
             _context = context;
         }
 
         [BindProperty]
         public SimpleAppUser SimpleAppUser { get; set; }
+        [BindProperty]
+        public bool EmailConfirmed { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
@@ -36,17 +44,28 @@ namespace SimpleApp.Areas.Identity.Pages.Users
             {
                 return NotFound();
             }
+            EmailConfirmed = SimpleAppUser.EmailConfirmed;
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
-            _context.Attach(SimpleAppUser).State = EntityState.Modified;
+            string id = SimpleAppUser.Id;
+            var user = _context.Users.Find(id);
+
+            if(EmailConfirmed)
+            {
+                await _userManager.AddToRoleAsync(user, SD.Admin);
+                user.EmailConfirmed = true;
+            }
+            else
+            {
+                await _userManager.RemoveFromRoleAsync(user, SD.Admin);
+                user.EmailConfirmed = false;
+            }
+            
+           // _context.Attach(SimpleAppUser).State = EntityState.Modified;
 
             try
             {
